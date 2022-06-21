7b8d2fdf446c61cb4bd1177237d0a365341d4a13
xukuan <xukuan@wxchina.com>
2022-06-21 20:06:47 +0800

    add get prefix kvs func

--- Ekv.go
+++ Ekv.go
@@ -1,1 +1,24 @@
 package srvDiscover
+
+import "bytes"
+
+type Ekv struct {
+	Key   []byte
+	Value []byte
+}
+
+func InitEkv(k, v []byte) Ekv {
+	return Ekv{k, v}
+}
+
+type Ekvs []Ekv
+
+func (list Ekvs) Len() int {
+	return len(list)
+}
+func (list Ekvs) Less(i, j int) bool {
+	return bytes.Compare(list[i].Key, list[j].Key) < 0
+}
+func (list Ekvs) Swap(i, j int) {
+	list[i], list[j] = list[j], list[i]
+}
